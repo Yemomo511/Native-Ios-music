78aaf8aab395ad09f66d14362f2e968e899de17e
yemomo <1463645455@qq.com>
2024-07-09 09:38:09 +0800

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by 叶墨沫 on 2024/7/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
